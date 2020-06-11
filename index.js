81baea1c77e023ab0bc3361ec55511cf0dbfa2c3
Aidan Hilt <aidanhilt2@gmail.com>
2020-06-11 17:06:45 -0400

    Added a solid layout for the beginning of the UI on the main page. Adding a menu to add new items to the business's inventory.

--- index.js
+++ index.js
@@ -1,20 +1,72 @@
-const { app, BrowserWindow } = require('electron');
+/*Importing libraries that we need*/
+const {ipcRenderer} = require('electron');
+const $ = require('jquery');
+const fs = require('fs');
 
-function createWindow () {
-  // Create the browser window.
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+//Setting global values that we will later use
+business = null;
+loadBusiness();
 
-  // and load the index.html of the app.
-  //win.removeMenu();
-  win.loadFile('index.html');
+/*Add functionality to the sliders to allow them to display 
+their values to the user.
+*/
+$("#fameLabel").text("Fame: " + document.getElementById("fameSlider").value);
+document.getElementById("fameSlider").addEventListener('input', function(){
+    $("#fameLabel").text("Fame: " + document.getElementById("fameSlider").value);
+    business.fame = document.getElementById("fameSlider").value;
+});
 
+$("#repLabel").text("Reputation: " + document.getElementById("repSlider").value);
+document.getElementById("repSlider").addEventListener('input', function(){
+    $("#repLabel").text("Reputation: " + document.getElementById("repSlider").value);
+    business.reputation = document.getElementById("repSlider").value;
+});
+
+$("#compLabel").text("Competition: " + document.getElementById("compSlider").value);
+document.getElementById("compSlider").addEventListener('input', function(){
+    $("#compLabel").text("Competition: " + document.getElementById("compSlider").value);
+    business.competition = document.getElementById("compSlider").value;
+});
+
+//Adding in the information of the loaded business
+updateUI(business);
+
+/**
+ * 
+ * @param {*} business A business object loaded from a JSON file that represents
+ * a business, its expenses, its products, and its general reputation in the city.
+ * 
+ * Updates the UI with new information about a business loaded from a file, or generated
+ * if no file could be found.
+ */
+function updateUI(business){
+    document.getElementById("fameSlider").value = business.fame;
+    $("#fameLabel").text("Fame: " + document.getElementById("fameSlider").value);
+
+    document.getElementById("repSlider").value = business.reputation;
+    $("#repLabel").text("Reputation: " + document.getElementById("repSlider").value);
+
+    document.getElementById("compSlider").value = business.competition;
+    $("#compLabel").text("Competition: " + document.getElementById("compSlider").value);
+
+    $("h1").text(business.name);
 }
 
-app.whenReady().then(createWindow);
+//================
+//Helper functions 
+//================
 
+function loadBusiness() {
+    if(fs.existsSync("default_business.json")){
+      business = JSON.parse(fs.readFileSync("default_business.json").toString());
+    }else{
+        business = {
+        name: "Default name",
+        debts: [],
+        products: [],
+        fame: 10,
+        reputation: 0,
+        competition: 0 //This value is unused in the current build 
+      }
+    }
+  }
